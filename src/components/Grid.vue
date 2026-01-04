be0ba7d7106253039456923f8dedfd1d09f89315
Silja Sihvo <silja.sihvo@gmail.com>
2026-01-04 22:21:35 +0100

    Add Grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,40 @@
+<script setup>
+import GridCard from "../components/GridCard.vue";
+
+defineProps({
+	id: String,
+});
+</script>
+
+<template>
+	<section class="grid-container">
+		<GridCard
+			imageSrc="../src/assets/images/chicken.png"
+			itemName="Chicken Breast"
+			quantity="500g"
+			storage="Fridge"
+			emoji="❄️" />
+
+		<GridCard
+			imageSrc="../src/assets/images/ground-beef.png"
+			itemName="Ground Beef"
+			quantity="350g"
+			storage="Fridge"
+			emoji="❄️" />
+
+		<GridCard
+			imageSrc="../src/assets/images/chicken-nuggets.png"
+			itemName="Chicken Nuggets"
+			quantity="1 pack"
+			storage="Freezer"
+			emoji="🧊" />
+	</section>
+</template>
+
+<style scoped>
+.grid-container {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 5px;
+}
+</style>
--- src/views/CategoryDetailView.vue
+++ src/views/CategoryDetailView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Searchbar from "../components/Searchbar.vue";
-import GridCard from "../components/GridCard.vue";
 import ViewToggle from "@/components/ViewToggle.vue";
+import Grid from "@/components/Grid.vue";
 
 defineProps({
 	id: String,
@@ -12,12 +12,7 @@ defineProps({
 	<Searchbar />
 	<h2>{{ id }}</h2>
 	<ViewToggle />
-	<GridCard
-		imageSrc="../src/assets/images/chicken.png"
-		itemName="Sample Item"
-		quantity="2 pcs"
-		storage="Fridge"
-		emoji="🥦" />
+	<Grid />
 </template>
 
 <style scoped></style>
