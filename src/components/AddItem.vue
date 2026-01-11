324349eafa5672f01ee27a3a3e7d7b951c180415
Silja Sihvo <silja.sihvo@gmail.com>
2026-01-11 12:06:41 +0100

    Add BaseInput component

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -1,7 +1,10 @@
 <script setup>
 import AddPhoto from "./AddPhoto.vue";
+import BaseInput from "./BaseInput.vue";
 </script>
 
 <template>
 	<AddPhoto />
+	<BaseInput label="Item Name" placeholder="e.g. Banana, Milk" />
+	<BaseInput label="Quantity" placeholder="e.g. 1L, 350g" />
 </template>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+	label: String,
+	placeholder: String,
+});
+</script>
+
+<template>
+	<label>{{ label }}</label>
+	<section class="container">
+		<input type="text" :placeholder="placeholder" />
+	</section>
+</template>
+
+<style scoped>
+.container {
+	background-color: var(--cards);
+	border-radius: 10px;
+	width: 100%;
+	box-sizing: border-box;
+	padding: 5px 0px;
+}
+
+label {
+	margin-bottom: 8px;
+	display: block;
+}
+
+input {
+	border: none;
+	border-radius: 10px;
+	padding: 12px 16px;
+	outline: none;
+}
+</style>
