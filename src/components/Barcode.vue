da5f178e90a429ae760504ace46f3aa94b58d5dd
Silja Sihvo <silja.sihvo@gmail.com>
2026-01-11 13:18:58 +0100

    Add ScanItemButton component

--- src/components/Barcode.vue
+++ src/components/Barcode.vue
@@ -1,7 +1,9 @@
 <script setup>
 import BarcodeScan from "./BarcodeScan.vue";
+import ScanItemButton from "./ScanItemButton.vue";
 </script>
 
 <template>
 	<BarcodeScan />
+	<ScanItemButton />
 </template>
--- src/components/BarcodeScan.vue
+++ src/components/BarcodeScan.vue
@@ -22,7 +22,7 @@
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	margin-top: 50px;
+	margin-top: 70px;
 }
 
 .barcode-img {
